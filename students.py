879f8ffc4000fff522f358741ca1a776f8aec145
Muciku <enjogu333@gmail.com>
2016-07-16 14:32:38 +0300

    Adding more data

--- students.py
+++ students.py
@@ -1,3 +1,6 @@
 student = {"name": ["Esther", "Njogu"], "DOB": 1900, "Tel": "0738937339"}
 
-
+print "My last name is {}".format(student.get("name")[1])
+print "My name is {}".format(student.get("name"))
+print "The cell number is {}".format(student.get("Tel"))
+print "I was born in the year " + "making me" + " years old"
