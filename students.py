66ea2bf93e173b2044701ceea3f2d9a9889f14b0
Muciku <enjogu333@gmail.com>
2016-07-16 15:24:32 +0300

    This states my age

--- students.py
+++ students.py
@@ -1,6 +1,11 @@
-student = {"name": ["Esther", "Njogu"], "DOB": 1900, "Tel": "0738937339"}
+import datetime
+student = {"name": ["Esther", "Njogu"], "Birthdate": datetime.date(1900,01,01), "Tel": "0733"}
 
 print "My last name is {}".format(student.get("name")[1])
-print "My name is {}".format(student.get("name"))
-print "The cell number is {}".format(student.get("Tel"))
-print "I was born in the year " + "making me" + " years old"
+print "While my full name is {} {}".format(student.get("name")[0], student.get("name")[1])
+# using print "My name is {}".format(student.get("name"))
+# the following will make the ouput come out as 
+#My name is ['Esther', 'Njogu']
+
+print "My cell number is {}".format(student.get("Tel"))
+print "I was born on {} ".format(student.get("Birthdate")) + "making me {} ".format(datetime.date.today().year-student.get("Birthdate").year) + " years old"
